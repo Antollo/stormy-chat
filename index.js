c721b9260e94df014f16f6c4a474ad66a5fc7e7b
Antoni Nowinowski <antoninowinowski@hotmail.com>
2018-03-04 18:35:04 +0100

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.port || 3000;
 var users = {};
 var conversations = {};
 
-app.use(expess.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
